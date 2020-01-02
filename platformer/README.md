c2ddbbc22b9b0cc7fa6e64da3aa8b6315c9ffcc1
leventarican <levent@protonmail.com>
2020-01-02 23:26:09 +0100

    init: 2D platformer

---
+++ platformer/README.md
@@ -0,0 +1,17 @@
+__scenes__
+* player: KinematicBody2D
+	* player: Sprite
+	* CollisionShape2D
+* level: Node2D
+	* TileMap
+	* Player
+
+__layers__
+* player
+* enemies
+* points
+* world
+
+__controlling__
+* Project Settings > Input Map
+	* move_down, move_up, jump, ...
---
+++ platformer/src/actors/player.gd
@@ -0,0 +1,30 @@
+extends Actor
+	
+func _physics_process(delta: float) -> void:
+	var is_jump_interrupted: = Input.is_action_just_released("jump") and _velocity.y < 0.0
+	var direction: = get_direction()
+	_velocity = calculate_move_velocity(_velocity, direction, speed, is_jump_interrupted)
+	var snap: Vector2 = Vector2.DOWN * 60.0 if direction.y == 0.0 else Vector2.ZERO
+	_velocity = move_and_slide_with_snap(
+		_velocity, snap, FLOOR_NORMAL, true
+	)
+
+func get_direction() -> Vector2:
+	return Vector2(
+		Input.get_action_strength("move_right") - Input.get_action_strength("move_left"),
+		-Input.get_action_strength("jump") if is_on_floor() and Input.is_action_just_pressed("jump") else 0.0
+	)
+
+func calculate_move_velocity(
+		linear_velocity: Vector2,
+		direction: Vector2,
+		speed: Vector2,
+		is_jump_interrupted: bool
+	) -> Vector2:
+	var new_velocity: = linear_velocity
+	new_velocity.x = speed.x * direction.x
+	if direction.y != 0.0:
+		new_velocity.y = speed.y * direction.y
+	if is_jump_interrupted:
+		new_velocity.y = 0.0
+	return new_velocity
