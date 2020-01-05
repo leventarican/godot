9a621a50c92bf8997be85f1df0307961b548b56a
leventarican <levent@protonmail.com>
2020-01-05 17:48:44 +0100

    movement, animation

--- platformer/src/Adventurer.gd
+++ platformer/src/Adventurer.gd
@@ -1,42 +1,35 @@
 extends KinematicBody2D
 
-# Declare member variables here. Examples:
-# var a: int = 2
-# var b: String = "text"
-
-export var speed: = Vector2(400.0, 500.0)
-export var gravity: = 3500.0
-
-var velocity: = Vector2.ZERO
-
+const SPEED = 400.0
+var character
+var boundary_max = 600
+var boundary_min = 0
 onready var animated_player: = get_node("Sprite/AnimationPlayer")
-# or access with $Sprite/AnimationPlayer
-onready var text_mode: = get_node("RichTextLabel")
 
-# Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
-	text_mode.text = "idle"
+	character = get_node(".")
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta: float) -> void:
-#	pass
-
-func _physics_process(delta: float) -> void:
-	
-	if Input.is_action_just_pressed("move_left"):
-		print('move left')
-		velocity.x -= 500.0
-	if Input.is_action_just_pressed("move_right"):
-		print('move right')
-		velocity.x += 500.0
+func _process(delta: float) -> void:
+	if Input.is_key_pressed(KEY_RIGHT):
+		move(1, delta)
+	if Input.is_key_pressed(KEY_LEFT):
+		move(-1, delta)
 		
 	if Input.is_action_just_pressed("attack"):
 		animated_player.play("attack")
-		text_mode.text = "attack"
 	if Input.is_action_just_pressed("jump"):
 		animated_player.play("jump")
-
-	#velocity.x += gravity * delta
-	move_and_slide(velocity)
-
+	
+func move(direction, delta):
+	if direction == 0:
+		return
+	
+	character.position += Vector2(SPEED, 0) * delta * direction
+	
+	if character.position.x > boundary_max:
+		character.position = Vector2(boundary_max, 0)
+	if character.position.x < boundary_min:
+		character.position = Vector2(boundary_min, 0)
+		
+	character.scale = Vector2(direction, 1)
+	
