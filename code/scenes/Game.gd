d508b8a79e998b121e01307ceae28ce219169f91
leventarican <levent@protonmail.com>
2020-05-24 04:20:31 +0200

    integrate camera and viewport to project

---
+++ code/scenes/Game.gd
@@ -0,0 +1,34 @@
+extends Node2D
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass
+
+func _process(delta):
+	zoom()
+
+# check InputMap for the key setup: zoom_in, zoom_out
+func zoom():
+	if Input.is_action_just_released('zoom_in'):
+		print('+')
+		$Camera2D.zoom.x -= 0.25
+		$Camera2D.zoom.y -= 0.25
+	if Input.is_action_just_released('zoom_out'):
+		print('-')
+		$Camera2D.zoom.x += 0.25
+		$Camera2D.zoom.y += 0.25
+
+# inspired by: https://www.youtube.com/watch?v=CqU6w164AbU&index=6&list=PLhqJJNjsQ7KEr_YlibZ3SBuzfw9xwGduK
+func offset():
+	var v = get_tree().root as Viewport
+	
+	# the same
+	var t = v.get_canvas_transform()
+	t = get_canvas_transform()
+	t = get_viewport().get_canvas_transform() as Transform2D
+	
+	print(t)
+	
+	# do some offset: The transform's translation offset.
+	t[2] = Vector2(-300.0, 100.0)
+	v.canvas_transform = t
